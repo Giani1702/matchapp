3fd45ec78fde192c0af121f37ac3e5b7edca8727
Konrad8181 <gizmooos8@wp.pl>
2024-07-14 20:57:00 +0200

    Add - Assets

---
+++ Assets/AppName.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchApp.Assets
+{
+    /// <summary>
+    /// Dummy class only for dynamic name-use
+    /// Use with nameof
+    /// </summary>
+    public class AthletLink
+    {
+    }
+}
